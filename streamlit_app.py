df5832c92d2d47f1085cf701e922ec2a15686ee2
Jojooyee <joeykerk1013@gmail.com>
2025-07-17 21:43:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -432,13 +432,13 @@ with tab2:
         simulated_df = pd.DataFrame(updated_rows)
         
         # Convert delivery time from days to hours
-        simulated_df['delivery_time_hour'] = simulated_df["delivery_time_days"] * 24
+        delivery_time_hour = simulated_df["delivery_time_days"] * 24
         # Calculate delivery speed (km/hour)
-        simulated_df["delivery_speed_kmph"] = simulated_df["distance_dc_to_user_km"] / simulated_df['delivery_time_hour']
+        delivery_speed_kmph = simulated_df["distance_dc_to_user_km"] / delivery_time_hour
         # Estimate new delivery time in hours using the same speed
-        simulated_df["estimated_new_delivery_time"] = simulated_df["distance_new_dc_to_user_km"] / simulated_df["delivery_speed_kmph"]
+        estimated_new_delivery_time = simulated_df["distance_new_dc_to_user_km"] / delivery_speed_kmph
         # Calculate improvement in hours
-        simulated_df["delivery_time_improvement"] = simulated_df["delivery_time_hour"] - simulated_df["estimated_new_delivery_time"]
+        simulated_df["delivery_time_improvement"] = delivery_time_hour - estimated_new_delivery_time
 
         simulated_processed = preprocessing_pipeline.transform(simulated_df)
 
